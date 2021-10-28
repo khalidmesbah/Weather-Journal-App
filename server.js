f88badc7327a3873900851e75e928a4a8ee80e0b
khalidMesbah <swekhalidmesbah6@gmail.com>
2021-10-29 00:13:22 +0200

    update the project

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+  name: "khaled",
+  age: 21,
+  color: "green",
+};
 
 // Require Express to run server and routes
 const express = require("express");
 
 // Start up an instance of app
 const app = express();
-const bodyParser = require("body-parser")
-const cors = require("cors")
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
 const port = 4000;
-function listening() {
-    console.log("hello"+port)
-}
-const server = app.listen(port, listening());
+app.listen(port, () => {
+  console.log(`the server is running on localhost ${port}`);
+});
+
+app.get("/all", (req, res) => {
+  res.send(projectData);
+  console.log(projectData);
+});
+
+app.post("/add", (req, res) => {
+  projectData = req.body;
+  console.log(projectData);
+  res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,103 @@
-/* Global Variables */
+/* ----------------Global Variables---------------- */
+// create the url
+let apiKey = `&appid=ddc41e9557902b7fefb029830d71fe4d`;
+let baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`;
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Create a date instance dynamically with JS
+let date = new Date().toDateString();
+
+// the absolute url server
+const server = "http://localhost:4000";
+
+// the button to generate the weather
+const generateBtn = document.getElementById("generate");
+
+// varibles to update the UI dynamiclly
+const dateEl = document.getElementById("date");
+const tempEl = document.getElementById("temp");
+const contentEl = document.getElementById("content");
+
+// error message for testing purposes
+const errorEl = document.querySelector(".holder.entry");
+
+// the info
+let info = {};
+
+// the feelings
+const feelings = document.getElementById("feelings");
+generateBtn.addEventListener("click", () => {
+  console.clear();
+  zipCode = document.getElementById("zip").value.trim();
+  console.log(baseUrl + zipCode + apiKey);
+  console.log(`=================`);
+  getWeatherData().then((data) => {
+    if (data) {
+      const {
+        main: { temp },
+        name: city,
+        weather: [{ description }],
+      } = data;
+
+      let info = {
+        data,
+        city,
+        temp: Math.round(temp),
+        description,
+        feelings: feelings.value || ` you did'nt enter your feelings`,
+      };
+      sendDataToTheServer(server + "/add", info);
+      console.log(info);
+      updateUI(info);
+    }
+  });
+});
+
+const getWeatherData = async () => {
+  try {
+    const respnose = await fetch(baseUrl + zipCode + apiKey);
+    const data = await respnose.json();
+
+    if (data.cod != 200) {
+      errorEl.innerHTML = data.message;
+      setTimeout(() => {
+        errorEl.style.display = "block";
+      }, 2000);
+      throw `${data.message}`;
+    }
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const sendDataToTheServer = async (url = "", info = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(info),
+  });
+
+  try {
+    const newData = await res.json();
+    console.log("You have saved" + newData);
+    return newData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateUI = async (info) => {
+  const res = await fetch(server + "/all");
+  try {
+    const info = await res.json();
+
+    dateEl.innerHTML = date;
+    tempEl.innerHTML = `${info.temp}`;
+    contentEl.innerHTML = info.feelings;
+  } catch (err) {
+    console.log(err);
+  }
+};
